3c80f0ba6bced49ec4e630f18834e1b3629cf5cf
yasinbagcuvan <yasinbagcuvan@gmail.com>
2024-03-25 14:54:42 +0300

    ++

---
+++ UygulamaSınav/Uygulama.BL/Manager/Concrete/KullaniciManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uygulama.BL.Manager.Abstract;
+using Uygulama.BL.MappingProfile;
+using Uygulama.BL.Models;
+using Uygulama.DAL.Context;
+using Uygulama.DAL.Entities;
+using Uygulama.DAL.Repository.Abstract;
+using Uygulama.DAL.Repository.Concrete;
+using Kullanici = Uygulama.DAL.Entities.Kullanici;
+
+namespace Uygulama.BL.Manager.Concrete
+{
+    public class KullaniciManager : Manager<KullaniciModel, Kullanici, KullaniciMapProfile>
+    {
+        public KullaniciManager()
+        {
+            _repository = new AlbumRepository(new UygulamaDbContext());
+        }
+
+    }
+}
